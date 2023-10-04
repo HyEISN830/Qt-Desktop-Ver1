42e9a478ce4eb56f93c0cb08c3fd9d623e7c872e
HyEISN <hyeisn@hotmail.com>
2023-10-04 21:25:54 +0800

    同属于日志修改相关

--- AppQmlBackend/qmlservice.h
+++ AppQmlBackend/qmlservice.h
@@ -8,6 +8,8 @@
 #include <QDesktopServices>
 #include <QSettings>
 #include <QTcpSocket>
+#include <QFile>
+#include <QDir>
 
 
 // qml 一般后台服务, 如append 到 剪贴板, 打开浏览器等..
@@ -24,6 +26,7 @@ public:
     Q_INVOKABLE QVariant takeSetting(QString key);
     Q_INVOKABLE void saveSettings(QString key, QString value);
     Q_INVOKABLE bool testTcpConn(QString host, int port);
+    Q_INVOKABLE bool saveContent2Log(QString name, QString content);
 
 private:
     QSettings *settings = nullptr;
