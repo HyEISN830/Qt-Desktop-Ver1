7223adbc7fb01619a548a09733de7048b9619d63
HyEISN <hyeisn@hotmail.com>
2023-10-04 11:45:14 +0800

    添加 TcpWorker 模块等相关文件
    1. 可以帮助连接到TCP Socket
    2. 可以自动连接以及自动重连, 间隔为五秒
    3. 可以多线程多TCP Socket 进行读写

---
+++ DeviceBackend/worker/tcpworker.h
@@ -0,0 +1,89 @@
+#ifndef TCPWORKER_H
+#define TCPWORKER_H
+
+#include <QThread>
+#include <QObject>
+#include <QTcpSocket>
+#include <QTimer>
+
+#include "tcpworkersender.h"
+
+
+class TcpWorker : public QThread
+{
+    Q_OBJECT
+public:
+    explicit TcpWorker(QObject *parent = nullptr);
+    TcpWorker(QString ip, int port) : ip(ip), port(port) {}
+
+    void run() override
+    {
+        tcp = new QTcpSocket;
+        connect(tcp, &QTcpSocket::readyRead, this, &TcpWorker::rx);
+        connect(tcp, &QTcpSocket::connected, this, &TcpWorker::connd);
+        connect(tcp, &QTcpSocket::disconnected, this, &TcpWorker::conndis);
+        connect(this, &QThread::finished, tcp, &QTcpSocket::deleteLater);
+
+        sender = new TcpWorkerSender;
+        sender->moveToThread(this);
+        connect(this, &TcpWorker::send, sender, &TcpWorkerSender::sendMsg2);
+        connect(this, &TcpWorker::sendb, sender , &TcpWorkerSender::sendMsg2b);
+        connect(this, &QThread::finished, sender, &TcpWorkerSender::deleteLater);
+
+        timer = new QTimer;
+        timer->moveToThread(this);
+        timer->setInterval(5000);
+        timer->start();
+        connect(timer, &QTimer::timeout, timer, [=] {   // tcp alive checking
+            if (!connecting && !conn)
+            {
+                bool ok = false;
+
+                connecting = true;
+                tcp->connectToHost(ip, port);
+                ok = tcp->waitForConnected(1000);
+                if (!ok) emit connectFailed();
+                connecting = false;
+            }
+        });
+        connect(this, &QThread::finished, timer, &QTimer::stop);
+        connect(this, &QThread::finished, timer, &QTimer::deleteLater);
+
+        connect(this, &QThread::finished, this, &QThread::deleteLater);
+        exec();
+    }
+
+    void write(QString content) { emit send(tcp, content); }
+    void write(QByteArray data) { emit sendb(tcp, data); }
+    void setIp(QString ip) { this->ip = ip; }
+    void setPort(int port) { this->port = port; }
+    void apply() { tcp->disconnectFromHost(); }     // call this function if change ip or port after
+
+signals:
+    void send(QTcpSocket*, QString);    // send string to tcp
+    void sendb(QTcpSocket*, QByteArray);    // send bytes to tcp
+    void received(QByteArray);
+    void sendError(QString);
+    void connected();
+    void connectFailed();
+    void disconnected();
+
+public slots:
+    void rx() { emit received(tcp->read(1024)); }
+    void serr(QString reason) { emit sendError(reason); }
+    void connd() { emit connected(); conn = true; }
+    void connf() { emit connectFailed(); conn = false; }
+    void conndis() { emit disconnected(); conn = false; }
+
+private:
+    QTcpSocket *tcp = nullptr;  // auto delete after used
+    QTimer *timer = nullptr;    // auto delete after used
+    TcpWorkerSender *sender = nullptr;  // auto delete after used
+    QString ip;
+    int port;
+
+    bool conn = false;
+    bool connecting = false;
+};
+
+#endif // TCPWORKER_H
